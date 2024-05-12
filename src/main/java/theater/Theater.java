77ab83d2b639bb90582f377f8da72dc51698f27b
khcdev <chakh9601@gmail.com>
2024-05-12 14:27:23 +0900

    [Chapter1] Theater가 ticketSeller의 인터페이스에만 의존하도록 수정(Bag, Ticket, TicketOffice 의존도 제거)

--- src/main/java/theater/Theater.java
+++ src/main/java/theater/Theater.java
@@ -7,27 +7,15 @@ public class Theater {
     public Theater(TicketSeller ticketSeller) {
         this.ticketSeller = ticketSeller;
     }
-    // 절차지향형 프로그램처럼 작성한 코드의 예시라고 볼 수 있음
-    /** 아래 방식의 문제점
-     * -> 결합도가 높음 (enter라는 프로세스를 기준으로 Bag, TicketSeller, TicketOffice, Ticket, Audience에 의존)
-     * -> 객체 자체가 독립적이지 못하고 Theater에 의해서 제어되는 '수동적인 형태'
-     *     ( 책에서 비유 한것은 티켓 판매원이 내 가방을 뒤져 초대권이 있는지 확인하고 돈을 빼는 것)
-     * -> ** 변경에 취약함
-     *     ( audience가 현금 말고 신용카드로 결제를 해야 한다면?, audience가 가방을 가지고 오지 않는다면? [전자발권 등])
-     *     ( 의존하고 있는 객체에 따라 Theater에도 계속 변경이 일어나야함 )
-     *
-     * -> 동작이 직관적이지 않음 (의사소통 비용 증가)
+
+    // 문제 해결1 : Theater가  tickerSeller의 인터페이스에만 의존하도록 수정
+    /**
+     * -> TicketSeller를 Encapsulation함으로써 객체가 독립적, 능동적으로 동작하게 함으로써 변경에 유연하지게 변경
      * */
     public void enter(Audience audience) {
-        if (audience.getBag().hasInvitation()) {
-            // ticketSeller가 TicketOffice에 대해 알고 있고, ticketOffice에서 티켓을 획득할 수 있음을 알아야함
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().setTicket(ticket);// audience의 Bag에서 ticket을 참조할수 있음을 알아야함
-        } else {
-            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
-            audience.getBag().minusAmount(ticket.getFee());
-            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
-            audience.getBag().setTicket(ticket);
-        }
+        // Theater와 ticketSeller는 `sellTo`라는 인터페이스를 통해서만 소통하게 됨
+        // Theater가 ticketSeller 내부 구현이 어떻게 되었는지는 이제 알 필요가 없음
+        // Encapsulation
+        ticketSeller.sellTo(audience);
     }
 }
--- src/main/java/theater/TicketSeller.java
+++ src/main/java/theater/TicketSeller.java
@@ -6,8 +6,18 @@ public class TicketSeller {
     public TicketSeller(TicketOffice ticketOffice) {
         this.ticketOffice = ticketOffice;
     }
+    // getOffice() 메소드 제거
 
-    public TicketOffice getTicketOffice() {
-        return ticketOffice;
+    public void sellTo(Audience audience) {
+        // ticketOffice의 접근은 TicketSeller를 통해서만 될 수 있도록 변경됨
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
     }
 }
